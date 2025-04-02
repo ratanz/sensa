8a5d74f3151a51cdc9f5eaa7dd972c7afc5e3ded
Ratan <ratanrathod870@gmail.com>
2025-04-02 20:30:21 +0530

    initial version of landing page text - v1 draft

--- app/pages/landing.tsx
+++ app/pages/landing.tsx
@@ -1,15 +1,61 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const landing = () => {
   return (
-    <div
-    className="w-full min-h-screen bg-[url(/assets/bg-sensa.avif)] bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center"
-  >
-    <h1 className="text-5xl font-bold text-white mb-6">Welcome to Sensa</h1>
-    <p className="text-2xl text-white">Your innovative next-gen platform</p>
-    
-  </div>
-  )
-}
+    <>
+      <div className="w-full h-full relative">
+        <Image
+          src="/assets/bg-sensa.avif"
+          alt=""
+          className="w-full h-full bg-center bg-cover opacity-20"
+          width={1000}
+          height={1000}
+        />
+      </div>
 
-export default landing
+      <div className="text-container ">
+        <div className="absolute w-70 top-1/3 left-30 text-white">
+          <h1 className="text-7xl font-bold border-b  border-white/20">
+            strategy
+          </h1>
+          <h1 className="text-7xl font-bold border-b border-white/20">
+            +craft
+          </h1>
+
+          <p className="mt-4 w-80 text-xl">
+            to create brands, products, and messages from first-timers to Zara,
+            BBC, Arco, Forbes... How? See the
+            <div className="flex gap-1">
+              <p className=" opacity-70 hover:opacity-100 transition-all duration-200 ease-in-out cursor-pointer">
+                work
+                <Image
+                  className="inline-flex ml-1"
+                  src="/assets/icon-1.png"
+                  width={15}
+                  height={15}
+                  alt=""
+                />
+              </p>
+
+              <span>or get an </span>
+              
+              <p className="opacity-70 hover:opacity-100 transition-all duration-200 ease-in-out cursor-pointer">
+                intro
+                <Image
+                  className="inline-flex ml-1"
+                  src="/assets/icon-2.png"
+                  width={15}
+                  height={15}
+                  alt=""
+                />
+              </p>
+            </div>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default landing;
