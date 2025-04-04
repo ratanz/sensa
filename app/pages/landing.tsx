5274f571793cb75c8c536f7a2f2b72fe8ff58506
Ratan <ratanrathod870@gmail.com>
2025-04-04 23:37:36 +0530

    code refactor and comments

--- app/pages/landing.tsx
+++ app/pages/landing.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 const landing = () => {
   return (
     <>
+
+      {/* bg image */}
       <div className="w-full h-full relative">
         <Image
           src="/assets/bg-sensa.avif"
@@ -14,7 +16,9 @@ const landing = () => {
         />
       </div>
 
+      {/* main-text */}
       <div className="text-container ">
+        {/* left side */}
         <div className="absolute w-70 top-1/3 left-30 text-white">
           <h1 className="text-7xl font-bold border-b  border-white/20">
             strategy
@@ -23,11 +27,14 @@ const landing = () => {
             +craft
           </h1>
 
-          <p className="mt-4 w-80 text-xl">
-            to create brands, products, and messages from first-timers to Zara,
-            BBC, Arco, Forbes... How? See the
+          <div className="mt-4 w-80 text-xl">
+            <p>
+              to create brands, products, and messages from first-timers to Zara,
+              BBC, Arco, Forbes... How? See the
+            </p>
+
             <div className="flex gap-1">
-              <p className=" opacity-70 hover:opacity-100 transition-all duration-300 ease-in-out cursor-pointer">
+              <p className=" opacity-80 hover:opacity-100 transition-all duration-300 ease-in-out cursor-pointer">
                 work
                 <Image
                   className="inline-flex ml-1"
@@ -39,7 +46,7 @@ const landing = () => {
               </p>
 
               <span>or get an </span>
-              
+
               <p className="opacity-70 hover:opacity-100 transition-all duration-300 ease-in-out cursor-pointer">
                 intro
                 <Image
@@ -51,8 +58,13 @@ const landing = () => {
                 />
               </p>
             </div>
-          </p>
+          </div>
         </div>
+
+        {/* right side */}
+
+
+
       </div>
     </>
   );
