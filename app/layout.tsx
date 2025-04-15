ab47e1d34ddfc47a91b2d35ec8b04b843d2eb137
Ratan <ratanrathod870@gmail.com>
2025-04-15 21:04:30 +0530

    added lenis scroll

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
+""
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ColorProvider } from "./context/ColorContext";
+import LenisScroll from "./providers/LenisScroll";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,11 +19,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className="antialiased"
       >
-        <ColorProvider>
-          {children}
-        </ColorProvider>
+        <LenisScroll>
+          <ColorProvider>
+            {children}
+          </ColorProvider>
+        </LenisScroll>
       </body>
     </html>
   );
