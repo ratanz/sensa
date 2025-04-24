111c3670a50535ab5be1b203a7a20bd7948fb6e3
Ratan <ratanrathod870@gmail.com>
2025-04-25 01:23:33 +0530

    Briteside image work done for big screen - test 1 v1 done

--- app/pages/Work.tsx
+++ app/pages/Work.tsx
@@ -1,53 +1,81 @@
-import { motion, useScroll, useTransform, cubicBezier, useSpring } from "framer-motion";
+import {
+  motion,
+  useScroll,
+  useTransform,
+  cubicBezier,
+  useSpring,
+} from "framer-motion";
 import { useRef } from "react";
 import Image from "next/image";
 import WorkTemplate from "../components/WorkTemplate";
 
 // Create individual project components using the WorkTemplate
 export const Klarna = () => {
-  
   const containerRef = useRef(null);
-  
+
   const { scrollYProgress } = useScroll({
     target: containerRef,
-    offset: ["start end", "end start"]
+    offset: ["start end", "end start"],
   });
-  
+
   // Apply spring physics to scrollYProgress for smoother transitions
   const smoothProgress = useSpring(scrollYProgress, {
     stiffness: 300,
     damping: 40,
     restDelta: 0.001,
-    mass: 0.5
+    mass: 0.5,
   });
-  
+
   // Transform values for container based on scroll with spring physics
-  const containerY = useTransform(
-    smoothProgress,
-    [0, 0.8],
-    [-50, -200],
-    { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) }
-  );
-  
+  const containerY = useTransform(smoothProgress, [0, 0.8], [-50, -200], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+
   // Transform values for card spreading with spring physics
-  const card1X = useTransform(smoothProgress, [0, 0.8], [-100, -500], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  const card1Y = useTransform(smoothProgress, [0, 0.8], [30, 120], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  const card1Rotate = useTransform(smoothProgress, [0, 0.8], [-2, -12], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  
-  const card2X = useTransform(smoothProgress, [0, 0.8], [-60, -280], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  const card2Y = useTransform(smoothProgress, [0, 0.8], [15, 60], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  const card2Rotate = useTransform(smoothProgress, [0, 0.8], [-1, -6], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  
-  const card4X = useTransform(smoothProgress, [0, 0.8], [60, 280], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  const card4Y = useTransform(smoothProgress, [0, 0.8], [15, 60], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  const card4Rotate = useTransform(smoothProgress, [0, 0.8], [1, 6], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  
-  const card5X = useTransform(smoothProgress, [0, 0.8], [100, 500], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  const card5Y = useTransform(smoothProgress, [0, 0.8], [30, 120], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  const card5Rotate = useTransform(smoothProgress, [0, 0.8], [2, 12], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
-  
+  const card1X = useTransform(smoothProgress, [0, 0.8], [-100, -500], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+  const card1Y = useTransform(smoothProgress, [0, 0.8], [30, 120], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+  const card1Rotate = useTransform(smoothProgress, [0, 0.8], [-2, -12], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+
+  const card2X = useTransform(smoothProgress, [0, 0.8], [-60, -280], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+  const card2Y = useTransform(smoothProgress, [0, 0.8], [15, 60], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+  const card2Rotate = useTransform(smoothProgress, [0, 0.8], [-1, -6], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+
+  const card4X = useTransform(smoothProgress, [0, 0.8], [60, 280], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+  const card4Y = useTransform(smoothProgress, [0, 0.8], [15, 60], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+  const card4Rotate = useTransform(smoothProgress, [0, 0.8], [1, 6], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+
+  const card5X = useTransform(smoothProgress, [0, 0.8], [100, 500], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+  const card5Y = useTransform(smoothProgress, [0, 0.8], [30, 120], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+  const card5Rotate = useTransform(smoothProgress, [0, 0.8], [2, 12], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
+
   // Center card subtle movement
-  const centerY = useTransform(smoothProgress, [0, 0.8], [0, -20], { ease: cubicBezier(0.32, 0.72, 0.35, 0.94) });
+  const centerY = useTransform(smoothProgress, [0, 0.8], [0, -20], {
+    ease: cubicBezier(0.32, 0.72, 0.35, 0.94),
+  });
 
   return (
     <WorkTemplate
@@ -64,33 +92,32 @@ export const Klarna = () => {
       accentColor="#610033"
       sectionColor="pink"
     >
-
       {/* Framer-style card layout with scroll-based spreading */}
-      <motion.div 
+      <motion.div
         ref={containerRef}
         className="absolute right-50 -bottom-[80vh] w-full h-full flex pointer-events-none"
         data-framer-name="klarna cards"
-        style={{ 
-          willChange: "transform", 
+        style={{
+          willChange: "transform",
           perspective: "1200px",
-          y: containerY
+          y: containerY,
         }}
       >
         {/* Card 1 - Cart info */}
-        <motion.div 
+        <motion.div
           className="absolute left-1/2"
           data-framer-name="card_1"
-          style={{ 
-            willChange: "transform", 
+          style={{
+            willChange: "transform",
             transformOrigin: "center",
             x: card1X,
             y: card1Y,
             rotate: card1Rotate,
             translateX: "-50%",
-            perspective: "1200px"
+            perspective: "1200px",
           }}
         >
-          <div 
+          <div
             style={{
               position: "absolute",
               borderRadius: "24px",
@@ -100,11 +127,11 @@ export const Klarna = () => {
               left: 0,
               width: "440px",
               height: "760px",
-              overflow: "hidden"
+              overflow: "hidden",
             }}
             data-framer-background-image-wrapper="true"
           >
-            <Image 
+            <Image
               src="https://framerusercontent.com/images/P4cQoW8nLK9NZ6jkeMtUBx8.png?scale-down-to=1024"
               alt="card cart info"
               width={770}
@@ -115,27 +142,27 @@ export const Klarna = () => {
                 height: "100%",
                 borderRadius: "inherit",
                 objectPosition: "center",
-                objectFit: "cover"
+                objectFit: "cover",
               }}
             />
           </div>
         </motion.div>
 
         {/* Card 5 - Checkout code */}
-        <motion.div 
+        <motion.div
           className="absolute left-1/2"
           data-framer-name="card_5"
-          style={{ 
-            willChange: "transform", 
+          style={{
+            willChange: "transform",
             transformOrigin: "center",
             x: card5X,
             y: card5Y,
             rotate: card5Rotate,
             translateX: "-50%",
-            perspective: "1200px"
+            perspective: "1200px",
           }}
         >
-          <div 
+          <div
             style={{
               position: "absolute",
               borderRadius: "24px",
@@ -145,11 +172,11 @@ export const Klarna = () => {
               left: 0,
               width: "440px",
               height: "760px",
-              overflow: "hidden"
+              overflow: "hidden",
             }}
             data-framer-background-image-wrapper="true"
           >
-            <Image 
+            <Image
               src="https://framerusercontent.com/images/BGi7zUoC7eHlvYnwdxd19FP3BqA.svg"
               alt="card checkout code"
               width={475}
@@ -160,28 +187,28 @@ export const Klarna = () => {
                 height: "100%",
                 borderRadius: "inherit",
                 objectPosition: "center",
-                objectFit: "cover"
+                objectFit: "cover",
               }}
             />
           </div>
         </motion.div>
 
         {/* Card 2 - Product page info */}
-        <motion.div 
+        <motion.div
           className="absolute left-1/2"
           data-framer-name="card_2"
-          style={{ 
-            willChange: "transform", 
+          style={{
+            willChange: "transform",
             transformOrigin: "center",
             zIndex: 2,
             x: card2X,
             y: card2Y,
             rotate: card2Rotate,
             translateX: "-50%",
-            perspective: "1200px"
+            perspective: "1200px",
           }}
         >
-          <div 
+          <div
             style={{
               position: "absolute",
               borderRadius: "24px",
@@ -191,11 +218,11 @@ export const Klarna = () => {
               left: 0,
               width: "440px",
               height: "760px",
-              overflow: "hidden"
+              overflow: "hidden",
             }}
             data-framer-background-image-wrapper="true"
           >
-            <Image 
+            <Image
               src="https://framerusercontent.com/images/J9LfzlF240RJ0MrTKIynPqOSBZ0.svg"
               alt="card product page info"
               width={475}
@@ -206,28 +233,28 @@ export const Klarna = () => {
                 height: "100%",
                 borderRadius: "inherit",
                 objectPosition: "center",
-                objectFit: "cover"
+                objectFit: "cover",
               }}
             />
           </div>
         </motion.div>
 
         {/* Card 4 - Checkout */}
-        <motion.div 
+        <motion.div
           className="absolute left-1/2"
           data-framer-name="card_4"
-          style={{ 
-            willChange: "transform", 
+          style={{
+            willChange: "transform",
             transformOrigin: "center",
             zIndex: 2,
             x: card4X,
             y: card4Y,
             rotate: card4Rotate,
             translateX: "-50%",
-            perspective: "1200px"
+            perspective: "1200px",
           }}
         >
-          <div 
+          <div
             style={{
               position: "absolute",
               borderRadius: "24px",
@@ -237,11 +264,11 @@ export const Klarna = () => {
               left: 0,
               width: "440px",
               height: "760px",
-              overflow: "hidden"
+              overflow: "hidden",
             }}
             data-framer-background-image-wrapper="true"
           >
-            <Image 
+            <Image
               src="https://framerusercontent.com/images/4a293gmFwxVPivadNyPAIyzjPw.svg"
               alt="card checkout"
               width={472}
@@ -252,26 +279,26 @@ export const Klarna = () => {
                 height: "100%",
                 borderRadius: "inherit",
                 objectPosition: "center",
-                objectFit: "cover"
+                objectFit: "cover",
               }}
             />
           </div>
         </motion.div>
 
         {/* Checkout handbook - Center card */}
-        <motion.div 
+        <motion.div
           className="absolute left-1/2"
           data-framer-name="checkout handbook"
-          style={{ 
-            willChange: "transform", 
+          style={{
+            willChange: "transform",
             transformOrigin: "center",
             zIndex: 2,
             y: centerY,
             translateX: "-50%",
-            perspective: "1200px"
+            perspective: "1200px",
           }}
         >
-          <div 
+          <div
             style={{
               position: "absolute",
               borderRadius: "24px",
@@ -281,11 +308,11 @@ export const Klarna = () => {
               left: 0,
               width: "440px",
               height: "760px",
-              overflow: "hidden"
+              overflow: "hidden",
             }}
             data-framer-background-image-wrapper="true"
           >
-            <Image 
+            <Image
               src="https://framerusercontent.com/images/R1HbpOq67pldX3FyVYBo04zaA2k.png?scale-down-to=1024"
               alt="card cover"
               width={770}
@@ -296,13 +323,12 @@ export const Klarna = () => {
                 height: "100%",
                 borderRadius: "inherit",
                 objectPosition: "center",
-                objectFit: "cover"
+                objectFit: "cover",
               }}
             />
           </div>
         </motion.div>
       </motion.div>
-
     </WorkTemplate>
   );
 };
@@ -311,38 +337,30 @@ export const BBC = () => {
   // BBC images
   const bbcImages = [
     "https://framerusercontent.com/images/AkYsZUpUCQt44M7tbrHcfQJ6Yto.png",
-    "https://framerusercontent.com/images/XZ9dQO5Y0Kfg5wSnMvcjQnibBEU.png"
+    "https://framerusercontent.com/images/XZ9dQO5Y0Kfg5wSnMvcjQnibBEU.png",
   ];
-  
+
   // Create refs for the container and elements
   const containerRef = useRef(null);
-  
+
   // Use Framer Motion's useScroll hook with enhanced options
   const { scrollYProgress } = useScroll({
     target: containerRef,
-    offset: ["start end", "end start"]
+    offset: ["start end", "end start"],
   });
-  
+
   // Apply spring physics to scrollYProgress for smoother transitions
   const smoothProgress = useSpring(scrollYProgress, {
     stiffness: 300,
     damping: 40,
     restDelta: 0.001,
-    mass: 0.5
+    mass: 0.5,
   });
-  
+
   // Transform the scroll progress into y-position values with built-in easing for smoother transitions
-  const image1Y = useTransform(
-    smoothProgress, 
-    [0, 1], 
-    [50, -580],
-  ); 
-  
-  const image2Y = useTransform(
-    smoothProgress, 
-    [0, 1], 
-    [0, -700],
-  );
+  const image1Y = useTransform(smoothProgress, [0, 1], [50, -580]);
+
+  const image2Y = useTransform(smoothProgress, [0, 1], [0, -700]);
 
   return (
     <WorkTemplate
@@ -359,51 +377,43 @@ export const BBC = () => {
       accentColor="#FFFFFF"
       sectionColor="darkBlue"
     >
-      <div ref={containerRef} className="absolute -left-10 px-2 w-screen h-full flex">
-          <motion.div 
-            className="w-1/2 relative top-70 left-8"
-            style={{ y: image1Y }}
-            initial={{ opacity: 1 }}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: false }}
-            transition={{ 
-              type: "spring", 
-              stiffness: 30, 
-              damping: 50,
-              mass:1.1,
-              restDelta: 0.001
-            }}
-          >
-            <Image
-              src={bbcImages[0]}
-              alt="image 1"
-              width={750}
-              height={100}
-            />
-          </motion.div>
+      <div
+        ref={containerRef}
+        className="absolute -left-10 px-2 w-screen h-full flex"
+      >
+        <motion.div
+          className="w-1/2 relative top-70 left-8"
+          style={{ y: image1Y }}
+          initial={{ opacity: 1 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: false }}
+          transition={{
+            type: "spring",
+            stiffness: 30,
+            damping: 50,
+            mass: 1.1,
+            restDelta: 0.001,
+          }}
+        >
+          <Image src={bbcImages[0]} alt="image 1" width={750} height={100} />
+        </motion.div>
 
-          <motion.div 
-            className="relative top-72 -left-10 w-full h-full"
-            style={{ y: image2Y }}
-            initial={{ opacity: 1}}
-            whileInView={{ opacity: 1 }}
-            viewport={{ once: false }}
-            transition={{ 
-              type: "spring", 
-              stiffness: 30, 
-              damping: 40,
-              mass: 1.3,
-              restDelta: 0.001
-            }}
-          >
-            <Image
-              src={bbcImages[1]}
-              alt="image 2"
-              width={1200}
-              height={90}
-            />
-          </motion.div>
-
+        <motion.div
+          className="relative top-72 -left-10 w-full h-full"
+          style={{ y: image2Y }}
+          initial={{ opacity: 1 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: false }}
+          transition={{
+            type: "spring",
+            stiffness: 30,
+            damping: 40,
+            mass: 1.3,
+            restDelta: 0.001,
+          }}
+        >
+          <Image src={bbcImages[1]} alt="image 2" width={1200} height={90} />
+        </motion.div>
       </div>
     </WorkTemplate>
   );
@@ -412,8 +422,29 @@ export const BBC = () => {
 export const Briteside = () => {
   // Briteside images
   const britesideImages = [
-    "https://framerusercontent.com/images/PfIhsgwZIoMJAOjvkLdOMXejM.jpg?scale-down-to=1024",
+    "https://framerusercontent.com/images/VU254B1mipsLwjH6l1PXVHz5lU.svg",
+    "https://framerusercontent.com/images/zQdP54C4i7XKOYyqmPVNQ1Y3Q.png",
   ];
+
+  const containerRef = useRef(null);
+
+  // Use Framer Motion's useScroll hook with enhanced options
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end start"],
+  });
+
+  // Apply spring physics to scrollYProgress for smoother transitions
+  const smoothProgress = useSpring(scrollYProgress, {
+    stiffness: 300,
+    damping: 40,
+    restDelta: 0.001,
+    mass: 0.5,
+  });
+
+  // Transform the scroll progress into y-position values with built-in easing for smoother transitions
+  const image1Y = useTransform(smoothProgress, [0, 1], [50, -230]);
+  const image2Y = useTransform(smoothProgress, [0, 1], [0, -230]);
 
   return (
     <WorkTemplate
@@ -430,7 +461,54 @@ export const Briteside = () => {
       accentColor="#047659"
       sectionColor="yellow"
     >
-   
+      <div ref={containerRef} className="absolute left-0 w-screen h-full flex ">
+        <motion.div
+          className="relative top-30 left-20"
+          style={{ y: image1Y }}
+          initial={{ opacity: 1 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: false }}
+          transition={{
+            type: "spring",
+            stiffness: 30,
+            damping: 50,
+            mass: 1.1,
+            restDelta: 0.001,
+          }}
+        >
+          <Image
+          className="w-[370vw]"
+            src={britesideImages[0]}
+            alt="image 1"
+            width={850}
+            height={100}
+          />
+        </motion.div>
+
+        <motion.div
+          className="relative top-48 right-30"
+          style={{ y: image2Y }}
+          initial={{ opacity: 1 }}
+          whileInView={{ opacity: 1 }}
+          viewport={{ once: false }}
+          transition={{
+            type: "spring",
+            stiffness: 30,
+            damping: 50,
+            mass: 1.1,
+            restDelta: 0.001,
+          }}
+        >
+          <Image
+          className="w-[170vw]"
+            src={britesideImages[1]}
+            alt="image 1"
+            width={750}
+            height={100}
+          />
+        </motion.div>
+
+      </div>
     </WorkTemplate>
   );
 };
@@ -457,8 +535,7 @@ export const Cabify = () => {
       }
       accentColor="#6039C8"
       sectionColor="lightPurple"
-    >
-    </WorkTemplate>
+    ></WorkTemplate>
   );
 };
 
@@ -483,8 +560,7 @@ export const Facebook = () => {
       }
       accentColor="#1F4E98"
       sectionColor="darkBlue"
-    >
-    </WorkTemplate>
+    ></WorkTemplate>
   );
 };
 
@@ -504,13 +580,13 @@ export const JME = () => {
       title="JME"
       subtitle={
         <>
-          Helping one of the top European VC <br /> firms to better tell what makes them different
+          Helping one of the top European VC <br /> firms to better tell what
+          makes them different
         </>
       }
       accentColor="#DFD0BB"
       sectionColor="navyBlue"
-    >
-    </WorkTemplate>
+    ></WorkTemplate>
   );
 };
 
